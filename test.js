0fcdda3183e31110b41f67a233b102a28887819c
Nurut Thoyibah <37695335+nurutthoyibah@users.noreply.github.com>
2018-05-11 21:27:40 +0700

    menambahkan append

---
+++ test.js
@@ -0,0 +1,11 @@
+master_mahass
++id_mahasiswa
++nim
++nama
+
+if(lampu_traffic == 'merah')
+    'mobil berhenti'
+elseif(lampu_traffic == 'kunig')
+    'mobil pelan'
+elseif(lampu_traffic == 'hijau')
+    'mobil jalan'
